b7fd753ad85d1dbbe28682f526b38072f97222e3
Gregory Arcentales <gregoarcenta@gmail.com>
2026-04-26 23:36:14 -0500

    Eliminar propiedad innecesaria `show-sql` en application.yaml.

--- backend/src/main/java/com/retrobbs/backend/infrastructure/config/SecurityConfig.java
+++ backend/src/main/java/com/retrobbs/backend/infrastructure/config/SecurityConfig.java
@@ -51,9 +51,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration config
-    ) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
 
